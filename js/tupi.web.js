531f4bc10355e5324f4f630c4b0c33d7a90e42a6
Sebastian Ortiz <neoecos@gmail.com>
2013-02-21 21:00:02 -0500

    Se agregaron los parámetros al post de tupitube.com 	Se modifico la paleta de colores

--- js/tupi.web.js
+++ js/tupi.web.js
@@ -14,24 +14,52 @@ paper.install(window);
 $(function() {
 	console.log("Installing PaperScript Global Scope");
 	paper.setup("canvas");
-	$('#submit-button').click(function() {		
-		$('#hiddenSVG').addClass('hasSVG');
-		var svg = paper.project.exportSvg();
-		//This should be done by the PaperScript
-		svg.setAttribute('width', $('#canvas').width());
-        svg.setAttribute('height', $('#canvas').height());
-        $('#hiddenSVG').empty();
-		$('#hiddenSVG').append(svg);		
-		var svg = $('<div>').append($('#hiddenSVG').children().clone()).html();
-		//var openURL = new MozActivity({name: "view",data: {type: "url", url:"http://tupitube.com"}});
-		//var shareActivity = new MozActivity({name: "share",data: {type: "url", url:"http://tupitube.com"}});		
-		$.post("http://tupitube.com/svg/upload/file",{'svg':svg})		
-		console.log("SVG:" + svg);
-
-	});
-
+	$('#submit-button').click(submitCanvas);
 })
 
+function submitCanvas() {
+    var tmpDiv = $('<div>');
+	var svg = paper.project.exportSvg();
+	//This should be done by the PaperScript
+	svg.setAttribute('width', $('#canvas').width());
+	svg.setAttribute('height', $('#canvas').height());
+
+	tmpDiv.empty();
+	tmpDiv.append(svg);
+	var svg = $('<div>').append(tmpDiv.children().clone()).html();
+	var postInfo = {
+		'title' : 'NotImplementedYet',
+		'version' : 'v0.1',
+		'author' : 'NotImplementedYet',
+		'description' : 'NotImplementedYet',
+		'svg' : svg
+	}
+	//var openURL = new MozActivity({name: "view",data: {type: "url", url:"http://tupitube.com"}});
+	//var shareActivity = new MozActivity({name: "share",data: {type: "url", url:"http://tupitube.com"}});
+	$.post("http://tupitube.com/svg/upload/file", postInfo, function(data, textStatus, jqHXR) {
+		console.log(textStatus);
+		console.log(data);
+	})
+}
+
+function showPalettePopup(){
+	console.log('Mostando Palette');
+	var palette = $('#popup');
+	 ColorPicker(
+        document.getElementById('popup'),
+        function(hex, hsv, rgb) {
+          console.log(hsv.h, hsv.s, hsv.v);         // [0-359], [0-1], [0-1]
+          console.log(rgb.r, rgb.g, rgb.b);         // [0-255], [0-255], [0-255]
+         	pathStyle.strokeColor = hex;        // #HEX
+         	$('#popup').hide();
+        });
+        
+	palette.show();
+}
+
+function closePalettePopup(){
+		$('#popup').hide();
+}
 function changeStyleColor() {
 	pathStyle.strokeColor = '#' + $('#button_palette').val();
 }
